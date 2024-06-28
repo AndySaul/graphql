17b2c0a0ecee8d9515fbec86bf961ac005bb12b7
Andy Saul <andycsaul@gmail.com>
2024-06-28 21:54:45 +0100

    Set GraphQL port from command line

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use anyhow::Result;
 use async_graphql::{http::GraphiQLSource, EmptyMutation, EmptySubscription, Object, Schema};
 use async_graphql_poem::*;
+use clap::Parser;
 use env_logger::Env;
 use log::info;
 use poem::{listener::TcpListener, web::Html, *};
@@ -29,14 +30,23 @@ fn init_logger() {
     );
 }
 
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// GraphQL port
+    #[arg(short, long, default_value_t = 54321, value_name("GRAPHQL PORT"))]
+    port: u16,
+}
+
 #[tokio::main]
 async fn main() -> Result<()> {
     init_logger();
 
+    let args = Args::parse();
+
     let schema = Schema::build(Query, EmptyMutation, EmptySubscription).finish();
 
-    let port: u16 = 55555;
-    let address: SocketAddr = (Ipv4Addr::LOCALHOST, port).into();
+    let address: SocketAddr = (Ipv4Addr::LOCALHOST, args.port).into();
     info!("GraphiQL: http://{address}");
 
     let app = Route::new().at("/", get(graphiql).post(GraphQL::new(schema)));
