3366f4e397f1e795d9cdc69a26217201cafdb883
Andy Saul <andycsaul@gmail.com>
2024-06-28 21:20:12 +0100

    Use logger

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use anyhow::Result;
 use async_graphql::{http::GraphiQLSource, EmptyMutation, EmptySubscription, Object, Schema};
 use async_graphql_poem::*;
+use env_logger::Env;
+use log::info;
 use poem::{listener::TcpListener, web::Html, *};
 
 use std::net::{Ipv4Addr, SocketAddr};
@@ -19,13 +21,23 @@ async fn graphiql() -> impl IntoResponse {
     Html(GraphiQLSource::build().finish())
 }
 
+fn init_logger() {
+    env_logger::init_from_env(
+        Env::default()
+            .filter_or("GRAPHQL_APP_LOG_LEVEL", "info")
+            .write_style_or("GRAPHQL_APP_LOG_STYLE", "always"),
+    );
+}
+
 #[tokio::main]
 async fn main() -> Result<()> {
+    init_logger();
+
     let schema = Schema::build(Query, EmptyMutation, EmptySubscription).finish();
 
     let port: u16 = 55555;
     let address: SocketAddr = (Ipv4Addr::LOCALHOST, port).into();
-    println!("GraphiQL: http://{address}");
+    info!("GraphiQL: http://{address}");
 
     let app = Route::new().at("/", get(graphiql).post(GraphQL::new(schema)));
 
